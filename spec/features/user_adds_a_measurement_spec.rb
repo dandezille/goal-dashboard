88ec343ec71d1c97ce395b9ccad9deb57be8397d
Dan de Zille <dan@ddez.net>
2020-04-11 21:28:53 +0100

    Cleaner test for adding a measurement

--- spec/features/user_adds_a_measurement_spec.rb
+++ spec/features/user_adds_a_measurement_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.feature 'User adds a measurement' do
-  scenario 'they see the measurement on the dashboard' do
+  scenario 'it is created' do
     visit root_path
     fill_form_and_submit(:measurement, attributes_for(:measurement))
-    expect(page).to have_css('.entry', text: '79.2')
+    expect(page).to have_css('.flash.notice', text: 'New measurement created')
+    expect(Measurement.count).to eq(1)
   end
 end
