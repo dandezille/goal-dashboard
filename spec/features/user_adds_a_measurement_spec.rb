e5565e5699108a3197d24032a7d7171849113484
Dan de Zille <dan@ddez.net>
2020-04-11 20:46:36 +0100

    Use locales to simplify markup and test

--- spec/features/user_adds_a_measurement_spec.rb
+++ spec/features/user_adds_a_measurement_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.feature "User add a measurement" do
   scenario 'they see the measurement on the dashboard' do
     visit root_path
-    fill_form(:measurement, { date: '10/04/20', value: '79.2' })
-    click_on 'Add'
+    fill_form_and_submit(:measurement, { date: '10/04/20', value: '79.2' })
     expect(page).to have_css('.entry', text: '79.2')
   end
 end
