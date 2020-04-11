3d95e72a7cef93cfd28054d091634b1cbe675657
Dan de Zille <dan@ddez.net>
2020-04-11 21:01:22 +0100

    Correct spec typo

--- spec/features/user_adds_a_measurement_spec.rb
+++ spec/features/user_adds_a_measurement_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.feature "User add a measurement" do
+RSpec.feature 'User adds a measurement' do
   scenario 'they see the measurement on the dashboard' do
     visit root_path
-    fill_form_and_submit(:measurement, { date: '10/04/20', value: '79.2' })
+    fill_form_and_submit(:measurement, attributes_for(:measurement))
     expect(page).to have_css('.entry', text: '79.2')
   end
 end
