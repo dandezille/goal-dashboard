24373af9f48c9b436981ce77ac9fd28a5c9e1f7e
Dan de Zille <dan@ddez.net>
2020-04-11 14:51:42 +0100

    Spec for measurement creation

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,7 +1,16 @@
 class MeasurementsController < ApplicationController
   def create
+    measurement = Measurement.new(measurement_params)
+    measurement.save
+    redirect_to root_path
   end
 
   def destroy
+  end
+
+  private
+
+  def measurement_params
+    params.require(:measurement).permit(:date, :value)
   end
 end
--- spec/requests/measurements_request_spec.rb
+++ spec/requests/measurements_request_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 RSpec.describe 'Measurements' do
 
   describe 'POST /measurements' do
-    it 'returns http success' do
-      post '/measurements'
-      expect(response).to have_http_status(:success)
+    it 'creates a measurement' do
+      measurement_attributes = attributes_for(:measurement)
+      post measurements_path, params: { measurement: measurement_attributes }
+
+      expect(response).to redirect_to(root_path)
+      expect(Measurement.count).to eq(1)
+
+      measurement = Measurement.first
+      expect(measurement.date).to eq(measurement_attributes[:date].to_date)
+      expect(measurement.value).to eq(measurement_attributes[:value])
     end
   end
 
