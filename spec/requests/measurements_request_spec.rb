21adfc40056219642e09a367c4f9aec2fd34d67f
Dan de Zille <dan@ddez.net>
2020-04-12 20:21:32 +0100

    Clean up measurement specs

--- spec/requests/measurements_request_spec.rb
+++ spec/requests/measurements_request_spec.rb
@@ -1,38 +1,44 @@
-require 'rails_helper'
+require 'rails_helper' 
 
 RSpec.describe 'Measurements' do
 
   describe 'POST /measurements' do
-    it 'creates a measurement' do
-      measurement_attributes = attributes_for(:measurement)
-      post measurements_path(as: create(:user)), params: { measurement: measurement_attributes }
-
-      expect(response).to redirect_to(root_path)
-      expect(Measurement.count).to eq(1)
-      expect(flash[:notice]).to be_present
-
-      measurement = Measurement.first
-      expect(measurement.date).to eq(measurement_attributes[:date].to_date)
-      expect(measurement.value).to eq(measurement_attributes[:value])
-    end
-
-    it 'shows errors for invalid input' do
-      post measurements_path(as: create(:user)), params: { measurement: attributes_for(:measurement, date: '') }
-      expect(response).to redirect_to(root_path)
-      expect(Measurement.count).to eq(0)
-      expect(flash[:alert]).to be_present
-    end
-
-    it 'requires a signed in user' do
+    it 'redirects if not signed in' do
       post measurements_path, params: { measurement: attributes_for(:measurement) }
       expect(response).to redirect_to(sign_in_path)
       expect(Measurement.count).to eq(0)
       expect(flash[:alert]).to be_present
     end
+
+    context 'when user signed in' do
+      before { sign_in }
+
+      it 'creates a measurement' do
+        measurement_attributes = attributes_for(:measurement)
+        post measurements_path, params: { measurement: measurement_attributes }
+
+        expect(response).to redirect_to(root_path)
+        expect(Measurement.count).to eq(1)
+        expect(flash[:notice]).to be_present
+
+        measurement = Measurement.first
+        expect(measurement.date).to eq(measurement_attributes[:date].to_date)
+        expect(measurement.value).to eq(measurement_attributes[:value])
+      end
+
+      it 'shows errors for invalid input' do
+        post measurements_path, params: { measurement: attributes_for(:measurement, date: '') }
+        expect(response).to redirect_to(root_path)
+        expect(Measurement.count).to eq(0)
+        expect(flash[:alert]).to be_present
+      end
+
+    end
   end
 
   describe 'DELETE /measurements/:id' do
     it 'deletes the given measurement' do
+      sign_in
       measurement = create(:measurement);
       delete measurement_path(measurement, as: create(:user))
 
@@ -41,7 +47,7 @@ RSpec.describe 'Measurements' do
       expect(flash[:notice]).to be_present
     end
 
-    it 'requires a signed in user' do
+    it 'redirects if not signed in' do
       measurement = create(:measurement);
       delete measurement_path(measurement)
 
@@ -49,5 +55,10 @@ RSpec.describe 'Measurements' do
       expect(Measurement.count).to eq(1)
       expect(flash[:alert]).to be_present
     end
+  end
+
+  def sign_in
+    @current_user = create(:user)
+    sign_in_as(@current_user)
   end
 end
