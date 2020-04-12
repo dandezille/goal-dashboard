bd480c267b5ea052c14b82303143d3f266e5e39f
Dan de Zille <dan@ddez.net>
2020-04-12 20:32:04 +0100

    Tidy deshboard request spec

--- spec/requests/dashboard_request_spec.rb
+++ spec/requests/dashboard_request_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "Dashboards", type: :request do
+RSpec.describe 'Dashboards' do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/"
-      expect(response).to have_http_status(:success)
+  describe 'GET /index' do
+    it 'is successful' do
+      get root_path
+      expect(response).to be_successful
     end
   end
 
