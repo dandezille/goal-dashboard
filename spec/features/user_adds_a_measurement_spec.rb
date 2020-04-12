06b5379b859d7d5a81eae977bb3748abba0d363c
Dan de Zille <dan@ddez.net>
2020-04-12 20:26:19 +0100

    Add helpers to feature specs

--- spec/features/user_adds_a_measurement_spec.rb
+++ spec/features/user_adds_a_measurement_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature 'User adds a measurement' do
+  before { sign_in }
+
   scenario 'it is created' do
-    visit root_path(as: create(:user))
+    visit root_path
     fill_form_and_submit(:measurement, attributes_for(:measurement))
     expect(page).to have_css('.flash.notice', text: 'Measurement created')
     expect(Measurement.count).to eq(1)
+  end
+
+  def sign_in
+    @current_user = create(:user)
+    visit root_path(as: @current_user)
   end
 end
--- spec/features/user_removes_a_measurement_spec.rb
+++ spec/features/user_removes_a_measurement_spec.rb
@@ -1,13 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature 'User removes a measurement' do
+  before { sign_in }
+
   scenario 'it is deleted' do
     measurement = create(:measurement)
-    visit root_path(as: create(:user))
+    visit root_path
     within("#measurement_#{measurement.id}") do
       click_on 'Delete'
     end
     expect(page).to have_css('.flash.notice', text: 'Measurement removed')
     expect(Measurement.count).to eq(0)
+  end
+
+  def sign_in
+    @current_user = create(:user)
+    visit root_path(as: @current_user)
   end
 end
