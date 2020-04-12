dfa0d4e9ab52ab4246b08519e8a379b9543d7272
Dan de Zille <dan@ddez.net>
2020-04-12 19:18:56 +0100

    Add user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
+  factory :user do
+    email { 'user@example.com' }
+    password { 'super_secret' }
+  end
+
   factory :measurement do
     date { '2020-04-10' }
     value { 78.2 }
   end
-
 end
