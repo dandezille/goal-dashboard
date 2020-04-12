1c09584bcfb1f2b3c70954f27939716335eba617
Dan de Zille <dan@ddez.net>
2020-04-12 16:11:27 +0100

    Dashboard requires sign in

--- spec/requests/dashboard_request_spec.rb
+++ spec/requests/dashboard_request_spec.rb
@@ -7,6 +7,11 @@ RSpec.describe 'Dashboards' do
       get root_path
       expect(response).to be_successful
     end
+
+    it "requires a signed in user" do
+      get root_path
+      expect(response).to redirect_to(sign_in_path)
+    end
   end
 
 end
