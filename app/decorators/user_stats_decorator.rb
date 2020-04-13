0b525901b6369fd57dd19cdbb14bcdee9da6338e
Dan de Zille <dan@ddez.net>
2020-04-13 16:05:35 +0100

    Use stats from decorator

--- app/decorators/user_stats_decorator.rb
+++ app/decorators/user_stats_decorator.rb
@@ -1,13 +1,27 @@
 class UserStatsDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
+  def pace
+    79.1
+  end
 
+  def daily_goal
+    0.17
+  end
+
+  def current
+    79.8
+  end
+
+  def to_go
+    3.8
+  end
+
+  def projected
+    77.5
+  end
+
+  def projected_date
+    '30/04/20'
+  end
 end
