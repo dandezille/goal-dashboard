d894e5377aa746d87107c0bc62385fa9bf4d6039
Dan de Zille <dan@ddez.net>
2020-04-15 21:23:26 +0100

    Handle updating an existing goal (#50)

--- spec/requests/goals_request_spec.rb
+++ spec/requests/goals_request_spec.rb
@@ -19,6 +19,22 @@ RSpec.describe 'Goals' do
         expect(goal.date).to eq(goal_attributes[:date].to_date)
         expect(goal.value).to eq(goal_attributes[:value])
       end
+
+      it 'clears an existing goal' do
+        sign_in_as(create(:user, :with_goal))
+
+        goal_attributes = attributes_for(:goal)
+        post goals_path, params: { goal: goal_attributes }
+
+        expect(response).to redirect_to(root_path)
+        expect(Goal.count).to eq(1)
+        expect(flash[:notice]).to be_present
+
+        goal = Goal.first
+        expect(goal.user).to eq(@current_user)
+        expect(goal.date).to eq(goal_attributes[:date].to_date)
+        expect(goal.value).to eq(goal_attributes[:value])
+      end
     end
     
     it 'redirects if not signed in' do
