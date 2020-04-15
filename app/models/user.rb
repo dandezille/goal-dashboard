7532cc7e251fa06a8690959308510d3469eb57ad
Dan de Zille <dan@ddez.net>
2020-04-15 19:06:44 +0100

    Add latest_measurement method to user (#45)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   include Clearance::User
   has_many(:measurements)
   has_one(:goal)
+
+  def latest_measurement
+    measurements.order(:date).first
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe User do
     it { is_expected.to validate_presence_of(:email) }
     it { is_expected.to validate_presence_of(:password) }
   end
+
+  describe '.latest_measurement', focus: true do
+    it 'returns the latest measurement' do
+      user = create(:user)
+      create(:measurement, user: user, date: Date.today - 2.days)
+      latest = create(:measurement, user: user, date: Date.today)
+      create(:measurement, user: user, date: Date.today - 1.day)
+
+      expect(user.latest_measurement).to eq(latest)
+    end
+  end
 end
