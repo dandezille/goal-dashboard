f68e93af30e5a222412261451aa87b1c6e0ce257
Dan de Zille <dan@ddez.net>
2020-04-18 19:09:16 +0100

    Add format_date helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def format_date(date)
+    date.strftime("%A #{date.day.ordinalize} %B")
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,4 +66,5 @@ RSpec.configure do |config|
   config.include SessionHelpers, type: :feature
   config.include ContentHelpers, type: :feature
   config.include SignInRequestHelpers, type: :request
+  config.include ApplicationHelper
 end
