cdc1e8c4fababdff8a6f39dc09fe794cb08e43aa
Dan de Zille <dan@ddez.net>
2020-05-03 20:57:22 +0100

    Test for and implement user destroying dependents

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   include Clearance::User
-  has_many(:measurements)
-  has_one(:goal)
+  has_many :measurements, dependent: :destroy
+  has_one :goal, dependent: :destroy
 
   def latest_measurement
     measurements.order(:date).first
