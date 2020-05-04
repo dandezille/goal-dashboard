3f31bb3e446ee4d82520f67d05356fa8ee656a62
Dan de Zille <dan@ddez.net>
2020-05-04 20:57:23 +0100

    Implement user decorator via behaviour modules

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,45 +1,83 @@
-class UserDecorator < ApplicationDecorator
-  def new_measurement
-    Measurement.new(date: Date.today)
-  end
-
+module UserWithGoalDecorator
   def goal
-    (model.goal || Goal.new).decorate
+    model.goal.decorate
   end
 
   def goal_description
-    return 'No goal set' unless model.goal
     goal.description
   end
 
   def target
-    return '?' unless model.goal
     goal.target
   end
 
   def target_delta
-    return '?' unless model.goal
     goal.target_delta
     end
 
   def daily_goal
-    return '?' unless model.goal
     goal.daily_goal
   end
 
   def to_go
-    return '?' unless model.goal
     goal.to_go
   end
 
   def projected_value
-    return '?' unless model.goal
     goal.projected_value
   end
 
   def projected_date
-    return '?' unless model.goal
     goal.projected_date
+  end
+end
+
+module UserWithoutGoalDecorator
+  def goal
+    Goal.new.decorate
+  end
+
+  def goal_description
+    'No goal set'
+  end
+
+  def target
+    return '?' 
+  end
+
+  def target_delta
+    return '?' 
+  end
+
+  def daily_goal
+    return '?' 
+  end
+
+  def to_go
+    return '?' 
+  end
+
+  def projected_value
+    return '?' 
+  end
+
+  def projected_date
+    return '?' 
+  end
+end
+
+class UserDecorator < ApplicationDecorator
+  def initialize(object, options = {})
+    super(object, options)
+    if object.goal
+      (class << self; include UserWithGoalDecorator; end)
+    else
+      (class << self; include UserWithoutGoalDecorator; end)
+    end
+  end
+
+  def new_measurement
+    Measurement.new(date: Date.today)
   end
 end
 
