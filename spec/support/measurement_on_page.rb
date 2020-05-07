684530338fd3e3428d320e995e123ca5aab489d6
Dan de Zille <dan@ddez.net>
2020-05-07 13:20:07 +0100

    Extract page object for measurement features (#98)
    * Use page object to clarify measurement feature specs

---
+++ spec/support/measurement_on_page.rb
@@ -0,0 +1,27 @@
+class MeasurementOnPage < Struct.new(:params)
+  include Capybara::DSL
+  include ApplicationHelper
+
+  def create
+    fill_in 'measurement_date', with: params[:date]
+    fill_in 'measurement_value', with: params[:value]
+    click_button 'Add'
+  end
+
+  def delete
+    page.find('.measurement') { |element|
+      match_measurement(element)
+    }.click_button 'Delete'
+  end
+
+  def visible?
+    page.has_css?('.measurement') { |element| match_measurement(element) }
+  end
+
+  private
+
+  def match_measurement(element)
+    element.has_css?('.value', text: params[:value]) &&
+      element.has_css?('.date', text: format_date(params[:date]))
+  end
+end
