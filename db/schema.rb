3df8993c00e5ada75b394688e10edb74946be897
Dan de Zille <dan@ddez.net>
2020-05-22 15:39:54 +0100

    Remove default units

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_22_143511) do
+ActiveRecord::Schema.define(version: 2020_05_22_143855) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_22_143511) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title", null: false
-    t.string "units", default: "kg", null: false
+    t.string "units", null: false
     t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
