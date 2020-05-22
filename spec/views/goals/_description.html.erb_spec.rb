f1d7a19e71941217693cfb877a9d0699df11e4db
Dan de Zille <dan@ddez.net>
2020-05-22 22:15:13 +0100

    Specs for whole description

--- spec/views/goals/_description.html.erb_spec.rb
+++ spec/views/goals/_description.html.erb_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'goals/_description.html.erb' do
+  let(:goal) { build(:goal, title: 'Weight', value: 72.32, date: '2020-05-02', units: 'kg', id: 0) }
+
   it 'displays a summary of the goal' do
-    goal = build(:goal, id: 0)
     render 'goals/description', goal: goal 
-    expect(rendered).to have_content goal.units
+    expect(rendered).to have_content 'Weight 72.3kg by 2nd May', normalize_ws: true
+  end
+
+  it 'shows link to edit goal' do
+    render 'goals/description', goal: goal
+    expect(rendered).to have_css "a[href=\"#{edit_goal_path(goal)}\"]"
   end
 end
