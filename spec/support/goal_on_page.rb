f5806d4d89b08955193d560d07e00bf0d78c0fd7
Dan de Zille <dan@ddez.net>
2020-05-22 15:55:23 +0100

    Extract out fill_fields method

--- spec/support/goal_on_page.rb
+++ spec/support/goal_on_page.rb
@@ -3,16 +3,14 @@ class GoalOnPage < Struct.new(:params)
   include FormatHelper
 
   def create
-    fill_in 'goal_title', with: params[:title]
-    fill_in 'goal_units', with: params[:units]
-    fill_in 'goal_date', with: params[:date]
-    fill_in 'goal_value', with: params[:value]
+    fill_fields
     click_button 'Update goal'
   end
 
   def edit
     page.find('#goal a').click
-    create
+    fill_fields
+    click_button 'Update goal'
   end
 
   def visible?
@@ -22,5 +20,14 @@ class GoalOnPage < Struct.new(:params)
         element.has_content?(params[:value]) &&
         element.has_content?(format_date(params[:date]))
     end
+  end
+
+  private
+
+  def fill_fields
+    fill_in 'goal_title', with: params[:title]
+    fill_in 'goal_units', with: params[:units]
+    fill_in 'goal_date', with: params[:date]
+    fill_in 'goal_value', with: params[:value]
   end
 end
