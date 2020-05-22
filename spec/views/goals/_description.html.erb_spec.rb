b36277a2b5f8df67ac70acd11a9a019b60eadaaf
Dan de Zille <dan@ddez.net>
2020-05-22 22:20:44 +0100

    Split specs

--- spec/views/goals/_description.html.erb_spec.rb
+++ spec/views/goals/_description.html.erb_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe 'goals/_description.html.erb' do
   let(:goal) { build(:goal, title: 'Weight', value: 72.32, date: '2020-05-02', units: 'kg', id: 0) }
 
-  it 'displays a summary of the goal' do
+  it 'shows the target' do
     render 'goals/description', goal: goal 
-    expect(rendered).to have_content 'Weight 72.3kg by 2nd May', normalize_ws: true
+    expect(rendered).to match /72.3kg/
+  end
+
+  it 'shows the date' do
+    render 'goals/description', goal: goal 
+    expect(rendered).to match /2nd May/
+  end
+
+  it 'shows the title' do
+    render 'goals/description', goal: goal 
+    expect(rendered).to match /Weight/
   end
 
   it 'shows link to edit goal' do
