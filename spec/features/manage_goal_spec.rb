5aa0a643de6a5b0e729623b4f85df1437d64bb12
Dan de Zille <dan@ddez.net>
2020-05-29 20:39:27 +0100

    Add goals index page (#134)
    * Add index page for goals

--- spec/features/manage_goal_spec.rb
+++ spec/features/manage_goal_spec.rb
@@ -3,8 +3,25 @@ require 'rails_helper'
 RSpec.feature 'manage goals' do
   let(:user) { create(:user) }
 
+  scenario 'view goals' do
+    goals = create_list(:goal, 3, user: user)
+
+    visit root_path(as: user)
+    within '.header' do
+      click_on 'Goals'
+    end
+
+    expect(page).to have_css('.card-title', text: 'Goals')
+
+    goals.each do |goal|
+      expect(page).to have_content goal.title
+    end
+  end
+
   scenario 'with no goal' do
     visit root_path(as: user)
+    expect(page).to have_css('.card-title', text: 'Create Goal')
+
     goal = goal_on_page
     goal.create
 
@@ -13,8 +30,10 @@ RSpec.feature 'manage goals' do
   end
 
   scenario 'with an existing goal' do
-    create(:goal, user: user)
+    goal = create(:goal, user: user)
     visit root_path(as: user)
+    click_on goal.title
+
     goal = goal_on_page
     goal.edit
 
