3c731aa2cc77d51657d7bc17ede5cfd032780921
Dan de Zille <dan@ddez.net>
2020-05-29 08:05:08 +0100

    Extract require goal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
+
+  def require_goal
+    @goal = Goal.find_by(id: params[:id])
+    if @goal.nil? || @goal.user != current_user
+      flash[:alert] = 'Invalid goal'
+      redirect_to goals_path
+    end
+  end
 end
