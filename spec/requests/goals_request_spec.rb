735ed12bd1aec910197800273d35e73853e68d5d
Dan de Zille <dan@ddez.net>
2020-07-20 13:58:16 +0100

    Tests for goal show assigns

--- spec/requests/goals_request_spec.rb
+++ spec/requests/goals_request_spec.rb
@@ -43,6 +43,15 @@ RSpec.describe 'Goals' do
         end
       end
 
+      it 'assigns goal' do
+        expect(assigns(:goal)).to eq(goal)
+      end
+
+      it 'assigns measurement' do
+        expect(assigns(:measurement)).to be_new_record
+        expect(assigns(:measurement).date).to eq(Date.today)
+      end
+
       context 'when user doesn\'t own the goal' do
         let(:goal) { create(:goal) }
         it { is_expected.to redirect_to(goals_path) }
