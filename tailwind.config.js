73353de331d45ccf8faa84a5f9dfffb40e035f13
Dan de Zille <dan@ddez.net>
2020-08-18 12:48:15 +0100

    Enable tailwind purge (#153)
    * Prettierignore
    * Tidy tailwind config, add purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
+  purge: ["./app/**/*.html.erb"],
+  theme: {
+    extend: {},
+  },
   variants: {
     backgroundColor: ["responsive", "hover", "focus", "even"],
   },
+  plugins: [],
 };
