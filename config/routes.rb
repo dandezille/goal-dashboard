d86d9861c8b8de5f997eedd130d35ed04abcae36
Dan de Zille <dan@ddez.net>
2020-12-30 14:43:23 +0000

    rails app:update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'goals#index'
-
-  resources :goals, only: %i[index show new create edit update] do
-    resources :measurements, only: %i[create destroy]
-  end
 end
